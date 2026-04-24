e49a582f8eaa385ad8c7ea34c459b9ab7288bb4e
initaman <amandabral2104@gmail.com>
2026-04-24 11:23:49 +0530

    add providers.tf

---
+++ infrastructure/terraform/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+  }
+  required_version = ">=1.6.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
